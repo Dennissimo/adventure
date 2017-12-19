e8b07358d3ee7574eb8239582f2194c7cb1e189c
dbiljard <dbiljard@bol.com>
2017-12-19 15:48:31 +0100

    Updated create class

--- create.py
+++ create.py
@@ -2,13 +2,17 @@
 
 class createCharacter:
 
-    def __init__(self,name,gender,playerClass):
+    def __init__(self,name,gender,playerClass,playerAgility=0,playerJumpProwess=0,playerSpeed=0):
         self.name = input("Welcome! State your name:")
         createCharacter.determine_name(self)
         self.gender = input("State your gender! Choose between M or F:")
         createCharacter.determine_gender(self)
-        self.playerClass = input("Choose your player class:")
+        self.playerClass = input("Choose your player class! Choose between [A]gile boii, [J]umpy boii or [F]asty boii:")
+        self.playerAgility = playerAgility
+        self.playerJumpProwess = playerJumpProwess
+        self.playerSpeed = playerSpeed
         createCharacter.determine_playerClass(self)
+
 
     def determine_name(self):
         # self.name = input("Welcome! State your name:")
@@ -23,9 +27,39 @@ class createCharacter:
             else:
                 print("Error with input. Please choose between M or F")
                 self.gender = input("State your gender! Choose between M or F:")
-                continue
+                #continue
 
     def determine_playerClass(self):
-        print("Your player class is " + self.playerClass)
+        while True:
+            if self.playerClass == 'A':
+                self.playerAgility += 4
+                self.playerJumpProwess += 2
+                self.playerSpeed += 2
+                print("Your player class is Agile boii")
+                createCharacter.print_attributes(self)
+                break
+            elif self.playerClass == 'J':
+                self.playerAgility += 2
+                self.playerJumpProwess += 4
+                self.playerSpeed += 2
+                print("Your player class is Jumpy boii")
+                createCharacter.print_attributes(self)
+                break
+            elif self.playerClass == 'F':
+                self.playerAgility += 2
+                self.playerJumpProwess += 2
+                self.playerSpeed += 4
+                print("Your player class is Fasty boii")
+                createCharacter.print_attributes(self)
+                break
+            else:
+                print("Error with input. Please choose between A, J or F")
+                self.playerClass = input("Choose your player class! Choose between [A]gile boii, [J]umpy boii or [F]asty boii:")
+
+
+    def print_attributes(self):
+        print("Your agility is " + str(self.playerAgility))
+        print("Your jump prowess is " + str(self.playerJumpProwess))
+        print("Your speed is " + str(self.playerSpeed))
 
 charInit = createCharacter("","","")
